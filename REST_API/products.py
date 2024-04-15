0fe642e3222900150ee4b0ca50f04e3c0e061afc
Alberthny <m.alberto.m.19@gmail.com>
2024-04-14 23:54:02 -0500

    API_p

---
+++ REST_API/products.py
@@ -0,0 +1,7 @@
+products = [
+    {"name": "motherboard", "price": "300", "quantity": "5"},
+    {"name": "HDD", "price": "100", "quantity": "3"},
+    {"name": "keyboard", "price": "50", "quantity": "8"},
+    
+
+]
